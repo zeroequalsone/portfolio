a01102e3efb1553bf80041227c279d529ae905c3
zeroequalsone <real.rizzle74@gmail.com>
2026-09-14 22:25:15 +0200

    feat(stack): add stack section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Stack from "@/components/Stack";
 import Work from "@/components/Work";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <main>
         <Hero />
         <Work />
+        <Stack />
       </main>
     </>
   );
